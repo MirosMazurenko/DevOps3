1758c4e3c81ad7fb79892f69e8fe0ba2bca1e8e5
Miros <mamiro18.12.2004@gmail.com>
2024-11-10 11:23:57 +0000

    Init commit

---
+++ funcA.h
@@ -0,0 +1,7 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+class FuncA {
+public:
+    double compute(double x, int n);
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "funcA.h"
+int main() {
+    FuncA func;
+    double x = 0.5;
+    int n = 3;
+    
+    double result = func.compute(x, n);
+    
+    std::cout << "The result of th(" << x << ") calculated using the first " << n << " terms is: " << result << std::endl;
+    return 0;
+}
